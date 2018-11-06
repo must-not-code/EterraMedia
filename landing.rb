7a0408d7d4c5cdca45003e9d772edb75d6783dd0
must-not-code <test4food@me.com>
2018-11-06 17:41:04 +0300

    add constellation

--- landing.rb
+++ landing.rb
@@ -2,38 +2,10 @@
 
 require 'sinatra'
 require 'slim'
+require 'rack-google-analytics'
+
+use Rack::GoogleAnalytics, tracker: ENV['GA_CODE']
 
 get '/' do
   slim :index
 end
-
-__END__
-@@ index
-html
-  head
-    meta charset='utf-8'
-    title E Terra
-    link href='/apple-touch-icon.png' rel='apple-touch-icon' sizes='180x180'
-    link href='/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png'
-    link href='/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png'
-  body
-    img.logo src='logo.png'
-css:
-  html {
-    background: url(../bg.png) no-repeat center center fixed;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-size: cover;
-  }
-
-  body {
-  }
-
-  .logo {
-    max-width: 100%;
-    position: absolute;
-    left: 0;
-    right: 0;
-    margin: auto;
-  }
